839f0dcd49ef0e59cf38fba48a496fd3358c2a74
Zifeng "Enrique" Su <suzihitaki@gmail.com>
2019-04-27 22:45:02 -0400

    Finish BV Complete List Updated

--- src/Table_CL.vue
+++ src/Table_CL.vue
@@ -62,7 +62,7 @@
                 {{ row.value.first }} {{ row.value.last }}
             </template>
 
-            <template slot="isActive" slot-scope="row">
+            <template slot="Banned" slot-scope="row">
                 {{ row.value ? 'Yes :)' : 'No :(' }}
             </template>
 
@@ -107,33 +107,33 @@
         data() {
             return {
                 items: [
-                    { isActive: true, age: 40, name: { first: 'Dickerson', last: 'Macdonald' } },
-                    { isActive: false, age: 21, name: { first: 'Larsen', last: 'Shaw' } },
+                    { Banned: true, age: 40, name: { first: 'Dickerson', last: 'Macdonald' } },
+                    { Banned: false, age: 21, name: { first: 'Larsen', last: 'Shaw' } },
                     {
-                        isActive: false,
+                        Banned: false,
                         age: 9,
                         name: { first: 'Mini', last: 'Navarro' },
                         _rowVariant: 'success'
                     },
-                    { isActive: false, age: 89, name: { first: 'Geneva', last: 'Wilson' } },
-                    { isActive: true, age: 38, name: { first: 'Jami', last: 'Carney' } },
-                    { isActive: false, age: 27, name: { first: 'Essie', last: 'Dunlap' } },
-                    { isActive: true, age: 40, name: { first: 'Thor', last: 'Macdonald' } },
+                    { Banned: false, age: 89, name: { first: 'Geneva', last: 'Wilson' } },
+                    { Banned: true, age: 38, name: { first: 'Jami', last: 'Carney' } },
+                    { Banned: false, age: 27, name: { first: 'Essie', last: 'Dunlap' } },
+                    { Banned: true, age: 40, name: { first: 'Thor', last: 'Macdonald' } },
                     {
-                        isActive: true,
+                        Banned: true,
                         age: 87,
                         name: { first: 'Larsen', last: 'Shaw' },
-                        _cellVariants: { age: 'danger', isActive: 'warning' }
+                        _cellVariants: { age: 'danger', Banned: 'warning' }
                     },
-                    { isActive: false, age: 26, name: { first: 'Mitzi', last: 'Navarro' } },
-                    { isActive: false, age: 22, name: { first: 'Genevieve', last: 'Wilson' } },
-                    { isActive: true, age: 38, name: { first: 'John', last: 'Carney' } },
-                    { isActive: false, age: 29, name: { first: 'Dick', last: 'Dunlap' } }
+                    { Banned: false, age: 26, name: { first: 'Mitzi', last: 'Navarro' } },
+                    { Banned: false, age: 22, name: { first: 'Genevieve', last: 'Wilson' } },
+                    { Banned: true, age: 38, name: { first: 'John', last: 'Carney' } },
+                    { Banned: false, age: 29, name: { first: 'Dick', last: 'Dunlap' } }
                 ],
                 fields: [
                     { key: 'name', label: 'Person Full name', sortable: true, sortDirection: 'desc' },
                     { key: 'age', label: 'Person age', sortable: true, class: 'text-center' },
-                    { key: 'isActive', label: 'is Active' },
+                    { key: 'Banned', label: 'is Active' },
                     { key: 'actions', label: 'Actions' }
                 ],
                 totalRows: 1,
